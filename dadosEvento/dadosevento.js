ec6a1eb573bcfb946e6de5cf5caca7e1c7fb5037
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-04-06 08:54:19 -0300

    Update dadoseevento.js
    Ajustando rotas de btn-login,

--- dadosEvento/dadosevento.js
+++ dadosEvento/dadosevento.js
@@ -5,6 +5,7 @@ var pedidos = document.getElementById("pedidos");
 pedidos.addEventListener("click", () => {
     window.location.assign("../paginaPedidos/paginapedidos.html")
 })
+
 btnBuscar.addEventListener("click", () => {
     window.location.assign("../paginaPratos/paginapratos.html");
 })
